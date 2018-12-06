1f8592beb7ada5648af9ee3e569e5da37bfee272
= <=>
2018-12-06 22:28:39 +0100

    indexcontroler

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Task;
 Auth::routes();
 
 Route::get('/index/{channel}', 'indexcontroller@index')->name('index');
-Route::get('/', 'IndexController@index')->name('home');
+Route::get('/', 'indexcontroller@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/create', 'TaskController@create')->name('create');
 Route::post('/store', 'TaskController@store')->name('store');
